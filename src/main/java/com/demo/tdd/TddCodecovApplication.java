4c0c1f86c31fea2016a7291e3a46b977b35ce3d4
Jeremy Sun <yunxuan_sun@apple.com>
2020-07-20 13:38:56 -0700

    first commit

---
+++ src/main/java/com/demo/tdd/TddCodecovApplication.java
@@ -0,0 +1,13 @@
+package com.demo.tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddCodecovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddCodecovApplication.class, args);
+	}
+
+}
